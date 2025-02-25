b458ba734c822ac95ea920ad3711a94a63a27364
Soumadip <39thdecember@gmail.com>
2025-02-25 20:05:44 +0530

    Add HeroSection on HomePage ✅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* USER LAYOUT */}
-        <Route path="/" element={<UserLayout />}></Route>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<HomePage/>}/>
+        </Route>
         {/* ADMIN LAYOUT */}
         <Route></Route>
       </Routes>
