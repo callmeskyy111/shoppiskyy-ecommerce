23130b7f84014a11637ef11c145f3456bfc5e418
Soumadip <39thdecember@gmail.com>
2025-02-25 21:47:05 +0530

    Add Gender-Collection section on HomePage ✔️

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
+import GenderCollection from '../components/Products/GenderCollection'
 
 function HomePage() {
   return (
     <div>
         <Hero/>
+        <GenderCollection/>
     </div>
   )
 }
